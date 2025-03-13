bfbfa70ede88228b9f2675ba57b77834427d12f1
anjaneya_sharma <anjaneya21449@iiitd.ac.in>
2025-03-13 20:58:26 +0530

    added initial Introductory Problems

---
+++ Introductory_Problems/NumberSpiral.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <limits>
+#include <climits>
+#include <math.h>
+#include <cmath>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+
+using ll = long long;
+const int M = 1e9 + 7;
+const int dx[4]{1,0,-1,0}, dy[4]{0,1,0,-1};
+
+ll gcd(ll a, ll b){
+    if (b == 0)
+       return a;
+    return gcd(b, a % b); 
+}
+
+template<class T> using pqg = priority_queue<T,vector<T>,greater<T>>;
+ll lcm(ll a, ll b) { return (a * b) / gcd(a, b) ; }
+
+using vi = vector<int>;
+using dvi = vector<vector<int>>;
+using vl = vector<ll>;
+using dvl = vector<vector<ll>>;
+using vc = vector<char>;
+using dvc = vector<vector<char>>;
+using vs = vector<string>;
+using dvs = vector<vector<string>>;
+
+#define sz(x) int((x).size())
+#define all(x) begin(x), end(x)
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+#define makp make_pair
+#define fir first
+#define sec second
+
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = a; i >= (b); --i)
+#define R0F(i,a) ROF(i,a,0)
+#define each(a,x) for (auto& a: x)
+
+// int N = 100005;
+// vl vec(N) , vis(N,0) , adj[100005];
+
+void solve(){
+	ll r, c;
+	cin >> r >> c;
+	if(r >= c){
+		if(r&1){
+			cout << (r-1)*(r-1) + c << "\n";
+		}	
+		else{
+			cout << r*r - c + 1 << "\n";
+		}
+	} 
+	else{
+		if(c & 1){
+			cout << c*c - r + 1 << "\n";
+		}
+		else cout << (c-1)*(c-1) + r << "\n"; 
+		
+	} 
+}
+
+
+int main(){
+  int t;
+  cin >> t;
+  while(t--){
+    solve();
+  }
+}
+
+
+
+
+
+
+
+
---
+++ Introductory_Problems/twoKnights.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <limits>
+#include <climits>
+#include <math.h>
+#include <cmath>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+
+using ll = long long;
+const int M = 1e9 + 7;
+const int dx[4]{1,0,-1,0}, dy[4]{0,1,0,-1};
+
+ll gcd(ll a, ll b){
+    if (b == 0)
+       return a;
+    return gcd(b, a % b); 
+}
+
+template<class T> using pqg = priority_queue<T,vector<T>,greater<T>>;
+ll lcm(ll a, ll b) { return (a * b) / gcd(a, b) ; }
+
+using vi = vector<int>;
+using dvi = vector<vector<int>>;
+using vl = vector<ll>;
+using dvl = vector<vector<ll>>;
+using vc = vector<char>;
+using dvc = vector<vector<char>>;
+using vs = vector<string>;
+using dvs = vector<vector<string>>;
+
+#define sz(x) int((x).size())
+#define all(x) begin(x), end(x)
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+#define makp make_pair
+#define f first
+#define s second
+
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = a; i >= (b); --i)
+#define R0F(i,a) ROF(i,a,0)
+#define each(a,x) for (auto& a: x)
+
+long long solve(int i){
+	ll sqtrm = 4*(i-1)*(i-2);
+	ll sq = i*i;
+	ll sqsq = (sq * (sq-1));
+	return (sqsq/2 - sqtrm);
+}
+
+int main(){
+	int n;
+	cin >> n;
+	FOR(i,1,n+1){
+		cout << solve(i) << endl;		
+	}
+}
+
+
+
+
+
+
